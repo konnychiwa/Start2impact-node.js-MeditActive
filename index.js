b378ee58c4535cc09d94a1bf645a35a4660ec485
Angelo Konara <angelokonara04@gmail.com>
2025-01-19 17:52:20 +0100

    All done

--- index.js
+++ index.js
@@ -1,27 +1,82 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.models.js');
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
-app.get('/tshirt', (req, res) => {
-  res.status(200).send({
-    tshirt: '👕',
-    size: 'Large',
-  });
+app.get('/', (req, res) => {
+  res.send('Hello from Node API');
 });
 
-app.post('/tshirt/:id', (req, res) => {
-  const { id } = req.params;
-  const { logo } = req.body;
-
-  if (!logo) {
-    res.status(418).send({ message: 'We need a logo!' });
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-
-  res.send({
-    tshirt: `👕 with your ${logo} and ID of ${id}`,
-  });
 });
 
-app.listen(PORT, () => console.log(`It's alive on http://localhost:${PORT}`));
+app.get('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post('/api/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json({ product });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// update product
+app.put('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// delete product
+app.delete('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    'mongodb+srv://Konny:m8mvNWNk0QIgpjd8@cluster0.vqx7n.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0'
+  )
+  .then(() => {
+    console.log('Connected!');
+    app.listen(PORT, () => {
+      console.log(`The server is hosted at http://localhost:${PORT}`);
+    });
+  });
